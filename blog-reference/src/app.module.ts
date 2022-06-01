46eee033d18f2f03aa44b7069d0610683837eab3
JeonSoongu <soonguprog@gmail.com>
2022-06-01 19:06:11 +0900

    refacotr dynamic-module option 인자 변경

--- blog-reference/src/app.module.ts
+++ blog-reference/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from './jwt/jwt.module';
+import { PASSWORD } from './jwt/password';
 
 @Module({
-  imports: [JwtModule.forRoot({ privateKey: 'string' })],
+  imports: [JwtModule.forRoot({ privateKey: PASSWORD })],
   controllers: [AppController],
   providers: [AppService],
 })
